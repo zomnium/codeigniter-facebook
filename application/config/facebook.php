4a782fe559c0c88492fa225a945d82e95800fd00
Tim van Bergenhenegouwen <tim@slurfhondje.nl>
2011-09-21 11:03:58 +0200

    Starting development
    Just merged the official Facebook php api files and changed the
    constructor.

---
+++ application/config/facebook.php
@@ -0,0 +1,8 @@
+<?php
+
+	$config['facebook_app_id'] 			= '162409213842185';
+	$config['facebook_api_secret'] 		= 'c7a493171e3cc16c871590004317d1be';
+
+//	$config['facebook_api_secret'] 		= 'ecf8045116f105b7c5cced3e1ae8b09c'; old
+	$config['facebook_default_scope']	= 'manage_pages'; // E.G 'read_stream,birthday,users_events,rsvp_event'
+	$config['facebook_api_url'] 		= 'https://graph.facebook.com/'; // Just in case it changes.
