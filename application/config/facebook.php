f1df02e53be8847912de949a5bec9e4569ff9f83
zomnium <tim@slurfhondje.nl>
2012-01-10 16:20:36 +0100

    Update application/config/facebook.php

--- application/config/facebook.php
+++ application/config/facebook.php
@@ -1,8 +1,7 @@
 <?php
 
-	$config['facebook_app_id'] 			= '162409213842185';
-	$config['facebook_api_secret'] 		= 'c7a493171e3cc16c871590004317d1be';
+	$config['facebook_app_id'] 		= ''; // Get one at: http://developers.facebook.com/
+	$config['facebook_api_secret'] 		= ''; // Get one at: http://developers.facebook.com/
 
-//	$config['facebook_api_secret'] 		= 'ecf8045116f105b7c5cced3e1ae8b09c'; old
 	$config['facebook_default_scope']	= 'manage_pages'; // E.G 'read_stream,birthday,users_events,rsvp_event'
 	$config['facebook_api_url'] 		= 'https://graph.facebook.com/'; // Just in case it changes.
